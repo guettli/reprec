ee97e1efc209a2564df56a5cf3e3bd044b65b17a
Bumpversion after CI <bumpversion-after-ci@example.com>
2017-11-06 11:49:23 +0000

    Bump version: 2017.23.0 -> 2017.24.0 [ci skip]

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
 
     # Updated via travisd: https://travis-ci.org/guettli/reprec
     # See .travis.yml
-    version='2017.23.0',
+    version='2017.24.0',
 
     description='reprec: Recursively replace strings in files and other goodies',
     long_description=long_description,

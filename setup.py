19279b9e16ea9a9d94a13e3cc9db1309bc05f27d
David Tiede <tidenhub@gmx.de>
2019-04-06 12:57:29 +0200

    Fix: Add Python 3 versions

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7
+        'Programming Language :: Python :: 3.7',
     ],
 
     packages=setuptools.find_packages(),

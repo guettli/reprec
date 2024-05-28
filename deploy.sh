fc1901f0126137f6a46e8183f6a676c1daceeab1
Thomas Guettler <info@thomas-guettler.de>
2024-05-28 22:27:11 +0200

    remove travis config

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-set -euxo pipefail
+trap 'echo "ERROR: A command has failed. Exiting the script. Line was ($0:$LINENO): $(sed -n "${LINENO}p" "$0")"; exit 3' ERR
+set -Eeuo pipefail
+
 rm -f dist/*
 python -m pytest
 bumpversion --verbose minor
 python setup.py sdist
 pip install twine
-twine upload --config-file .pypirc-bot dist/*
-
+#twine upload --config-file .pypirc-bot dist/*
+twine upload dist/*
